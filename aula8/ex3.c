f797f2a48dc71ad03601ab3dba48652cd0862ebe
GuilhermeCasal <guilherme.martins4gm@gmail.com>
2022-06-03 10:46:13 +0100

    update

--- aula8/ex3.c
+++ aula8/ex3.c
@@ -1,32 +1,28 @@
 #include <detpic32.h>
 
-volatile int count =  0;
+    volatile int count=0;
 
-int main(void)
- {
-
+int main(void){
     T3CONbits.TCKPS = 7;    // 1:256 prescaler Fout_presc = 78125;
-    PR3 = 39062;            // Fout = 20Mhz / (256 * (39062 + 1)) = 1,9999Hz
+    PR3 = 39063;            // Fout = 20Mhz / (256 * (39063 + 1)) = 1,999Hz
     TMR3 = 0;               // Reset timer T3 count register
-    T3CONbits.TON = 1;      // Enable timer T3   
+    T3CONbits.TON = 1;      // Enable timer T3  
 
     IPC3bits.T3IP = 2;      // Interrupt priority 2
     IEC0bits.T3IE = 1;      // Enable timer T3 interrupts
     IFS0bits.T3IF = 0;      // Reset timer T3 interrupt flag 
 
-
     EnableInterrupts();
     while(1);
-
     return 0;
- }
- void _int_(12) isr_T3(void) // Replace VECTOR by the timer T3
-  {                              // vector number
+}
+
+void _int_(12) isr_T3(void){
+
     count++;
-    if(count == 2){
+    if(count==2){
         putChar('.');
-        count = 0;
+        count=0;
     }
- 
-    IFS0bits.T3IF = 0;      // Reset T3 interrupt flag
-  } 
+    IFS0bits.T3IF = 0;
+}
